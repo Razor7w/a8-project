1350cd4a343d297af36d6e00b6e4810ef0e62769
scarroza <scarroza@widefense.com>
2021-01-10 18:25:18 -0300

    [Feat] - Se agrega ruta Hija

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component'
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import  { AuthGuard } from './guards/auth.guard'
+import { ExampleChildrenComponent } from './components/example-children/example-children.component'
 
 
 const routes: Routes = [
-  { path: 'home',
-    component: DashboardComponent,
-    canActivate: [AuthGuard]
+  { path: 'home', component: DashboardComponent, canActivate: [AuthGuard],
+    children: [
+      { path: "", component: ExampleChildrenComponent}
+    ]
   },
   { path: 'login', component: LoginComponent},
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
