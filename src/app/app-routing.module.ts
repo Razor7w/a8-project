afbfef6390cbcd1c0002e54dcaf2b046d85e4290
scarroza <scarroza@widefense.com>
2021-01-10 17:53:08 -0300

    [Feat] - Se agrega Guard en la ruta del dashboard para solo entrar logeado - Se implementa el lazyloading en el componente "page404"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Page404Component } from './components/page404/page404.component'
 import { LoginComponent } from './components/login/login.component'
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import  { AuthGuard } from './guards/auth.guard'
 
 
 const routes: Routes = [
-  { path: 'home', component: DashboardComponent},
+  { path: 'home',
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
   { path: 'login', component: LoginComponent},
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', component: Page404Component}
+  { path: '**', loadChildren: () => import('./components/page404/page404.module').then(m => m.Page404Module)}
 ];
 
 @NgModule({
